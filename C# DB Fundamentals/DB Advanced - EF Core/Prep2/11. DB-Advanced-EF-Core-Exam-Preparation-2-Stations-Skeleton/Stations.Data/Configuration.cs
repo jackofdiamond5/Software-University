ba6cdd3ac4aa4e833ccfb4363f9655649fd2f458
jackofdiamond5 <jackofdiamond596@gmail.com>
2017-12-19 13:54:02 +0200

    Removed unneeded directory

--- C# DB Fundamentals/DB Advanced - EF Core/Prep2/11. DB-Advanced-EF-Core-Exam-Preparation-2-Stations-Skeleton/Stations.Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-namespace Stations.Data
-{
-	public static class Configuration
-	{
-		public static string ConnectionString = @"Server=DESKTOP-1NK1I5O\SQLEXPRESS;Database=Stations;Trusted_Connection=True";
-	}
-}
