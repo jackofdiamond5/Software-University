c58f59434f586214f79ba4dbbb18bc053ad101ac
jackofdiamond5 <jackofdiamond596@gmail.com>
2018-03-03 23:02:16 +0200

    Updating DB Basics

---
+++ C# DB Fundamentals/DB Basics - T-SQL/Data Definition and Data Types/04. Insert Records in Both Tables.sql
@@ -0,0 +1,7 @@
+INSERT Towns (Id, Name) VALUES (1, 'Sofia');
+INSERT Towns (Id, Name) VALUES (2, 'Plovdiv');
+INSERT Towns (Id, Name) VALUES (3, 'Varna');
+
+INSERT Minions (Id, Name, Age, TownId) VALUES(1, 'Kevin', 22, 1);
+INSERT Minions (Id, Name, Age, TownId) VALUES(2, 'Bob', 15, 3);
+INSERT Minions (Id, Name, Age, TownId) VALUES(3,'Steward', NULL, 2);
