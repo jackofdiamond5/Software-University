5d006fe5434004eb793fde4f4993cf57d5677a02
jod <jackofdiamond596@gmail.com>
2018-03-28 13:16:25 +0300

    Uploading C# OOP Advanced Homework - Generics Homework

---
+++ C# Fundamentals/C# OOP Advanced/Generics/Generics_Exer/Ex. 1 - Generic Box/Box.cs
@@ -0,0 +1,14 @@
+﻿public class Box<T>
+{
+    public Box(T value)
+    {
+        this.ValueHolder = value;
+    }
+
+    public T ValueHolder { get; }
+
+    public override string ToString()
+    {
+        return $"{this.ValueHolder.GetType().FullName}: {this.ValueHolder}";
+    }
+}
---
+++ C# Fundamentals/C# OOP Advanced/Generics/Generics_Exer/Ex. 1 - Generic Box/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    static void Main()
+    {
+        GenericBoxOfInt();
+    }
+
+    /// <summary>
+    /// Task 2
+    /// </summary>
+    public static void GenericBoxOfInt()
+    {
+        var n = int.Parse(Console.ReadLine());
+
+        var boxes = new Queue<Box<int>>();
+
+        for (var i = 0; i < n; i++)
+        {
+            var input = int.Parse(Console.ReadLine());
+            var box = new Box<int>(input);
+            boxes.Enqueue(box);
+        }
+
+        foreach (var box in boxes)
+        {
+            Console.WriteLine(box);
+        }
+    }
+
+    /// <summary>
+    /// Task 1
+    /// </summary>
+    public static void GenericBoxOfString()
+    {
+        var n = int.Parse(Console.ReadLine());
+
+        var boxes = new Queue<Box<string>>();
+
+        for (var i = 0; i < n; i++)
+        {
+            var input = Console.ReadLine();
+            var box = new Box<string>(input);
+            boxes.Enqueue(box);
+        }
+
+        foreach (var box in boxes)
+        {
+            Console.WriteLine(box);
+        }
+    }
+}
