67eb0fb8ae2f9fee0863a2636c9e40ad6219c3e2
jod <jackofdiamond596@gmail.com>
2018-03-31 18:20:48 +0300

    Updating Iterators and Comparators homework

---
+++ C# Fundamentals/C# OOP Advanced/Iterators and Comparators/IterAndCompar_Exer/Ex. 7 - Equal Logic/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class Person : IComparable<Person>
+{
+    public Person(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+
+    public string Name { get; }
+
+    public int Age { get; }
+
+    public override bool Equals(object obj)
+    {
+        var person = (Person)obj;
+        return this.Name.Equals(person.Name) && this.Age.Equals(person.Age);
+    }
+
+    public override int GetHashCode()
+    {
+        unchecked
+        {
+            int hash = 13;
+            hash = (hash * 7) + (!object.ReferenceEquals(null, this.Name) ? this.Name.GetHashCode() : 0);
+            hash = (hash * 7) + (!object.ReferenceEquals(null, this.Age) ? this.Age.GetHashCode() : 0);
+            return hash;
+        }
+    }
+
+    public int CompareTo(Person other)
+    {
+        var result = this.Name.CompareTo(other.Name);
+
+        if (result == 0)
+        {
+            result = this.Age.CompareTo(other.Age);
+        }
+
+        return result;
+    }
+}
---
+++ C# Fundamentals/C# OOP Advanced/Iterators and Comparators/IterAndCompar_Exer/Ex. 7 - Equal Logic/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    static void Main()
+    {
+        var hashCol = new HashSet<Person>();
+        var sortedCol = new SortedSet<Person>();
+
+        var n = int.Parse(Console.ReadLine());
+        for(var i = 0; i < n; i++)
+        {
+            var args = Console.ReadLine().Split();
+            var person = new Person(args[0], int.Parse(args[1]));
+
+            hashCol.Add(person);
+
+            sortedCol.Add(person);
+        }
+
+        Console.WriteLine(sortedCol.Count);
+        Console.WriteLine(hashCol.Count);
+    }
+}
