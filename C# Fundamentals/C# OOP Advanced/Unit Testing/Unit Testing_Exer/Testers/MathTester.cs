dab93fbceef5e038dcac48d0f20791cac2c81ef7
boris <jackofdiamond596@gmail.com>
2018-04-06 15:57:53 +0300

    Updating homework for Unit Testing

---
+++ C# Fundamentals/C# OOP Advanced/Unit Testing/Unit Testing_Exer/Testers/MathTester.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using NUnit.Framework;
+
+public class MathTester
+{
+    [Test]
+    public void Abs_ReturnsModuleOfGivenNumber()
+    {
+        var numberToTest = -3;
+
+        Assert.That(() => Math.Abs(numberToTest),
+            Is.GreaterThan(0));
+    }
+
+    [Test]
+    public void Floor_RoundsToLowerBoundary()
+    {
+        var numberToTest = 5.9;
+        var expectedResult = 5;
+
+        Assert.That(() => Math.Floor(numberToTest),
+            Is.EqualTo(expectedResult));
+    }
+}
