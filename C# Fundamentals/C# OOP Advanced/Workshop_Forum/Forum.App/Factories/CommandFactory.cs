98cb690363ca6cf13cf12ced88700c1b6acba912
jod <jackofdiamond596@gmail.com>
2018-04-10 20:57:38 +0300

    Uploading Workshop

---
+++ C# Fundamentals/C# OOP Advanced/Workshop_Forum/Forum.App/Factories/CommandFactory.cs
@@ -0,0 +1,48 @@
+﻿namespace Forum.App.Factories
+{
+    using Contracts;
+
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class CommandFactory : ICommandFactory
+    {
+        IServiceProvider serviceProvider;
+
+        public CommandFactory(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+
+        public ICommand CreateCommand(string commandName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var commandType = assembly
+                .GetTypes()
+                .FirstOrDefault(t => t.Name == $"{commandName}Command");
+
+            if (commandType == null)
+            {
+                throw new InvalidOperationException("Invalid command!");
+            }
+
+            if (!typeof(ICommand).IsInstanceOfType(commandType))
+            {
+                throw new InvalidOperationException($"{commandName} is not a command!");
+            }
+
+            var ctorParams = commandType.GetConstructors().First().GetParameters();
+            var args = new object[] { ctorParams.Length };
+
+            for(var i = 0; i < args.Length; i++)
+            {
+                args[i] = this.serviceProvider.GetService(ctorParams[i].ParameterType);
+            }
+
+            var command = (ICommand)Activator.CreateInstance(commandType, args);
+
+            return command;
+        }
+    }
+}
---
+++ C# Fundamentals/C# OOP Advanced/Workshop_Forum/Forum.App/Factories/MenuFactory.cs
@@ -0,0 +1,43 @@
+﻿namespace Forum.App.Factories
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    using Forum.App.Contracts;
+
+    public class MenuFactory : IMenuFactory
+    {
+        IServiceProvider serviceProvider;
+
+        public MenuFactory(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+
+        public IMenu CreateMenu(string menuName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var menuType = assembly
+                .GetTypes()
+                .FirstOrDefault(t => t.Name == $"{menuName}Menu");
+
+            if(menuType == null)
+            {
+                throw new InvalidOperationException("Menu not found!");
+            }
+
+            var ctorParams = menuType.GetConstructors().First().GetParameters();
+            var args = new object[] { ctorParams.Length };
+
+            for(var i = 0; i < args.Length; i++)
+            {
+                args[i] = this.serviceProvider.GetService(ctorParams[i].ParameterType);
+            }
+
+            var menu = (IMenu)Activator.CreateInstance(menuType, args);
+
+            return menu;
+        }
+    }
+}
